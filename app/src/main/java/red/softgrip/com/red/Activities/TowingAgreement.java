06a35998486c5329a1c87d525bd6aa0da46c6b63
Qaiser Pasha <qaisershahzad216@gmail.com>
2018-09-06 22:11:54 +0500

    black popup

--- app/src/main/java/red/softgrip/com/red/Activities/TowingAgreement.java
+++ app/src/main/java/red/softgrip/com/red/Activities/TowingAgreement.java
@@ -1,15 +1,59 @@
 package red.softgrip.com.red.Activities;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import red.softgrip.com.red.R;
 
 public class TowingAgreement extends AppCompatActivity {
 
+    Button buttonAgreement;
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_towing_agreement);
+
+
+        buttonAgreement=(Button)findViewById(R.id.btn_yess);
+        buttonAgreement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowPopup();
+            }
+        });
+
+        myDialog = new Dialog(this);
+    }
+
+
+
+    public void ShowPopup() {
+        TextView txtclose;
+        Button btnOk;
+        myDialog.setContentView(R.layout.popup_design);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        btnOk = (Button) myDialog.findViewById(R.id.btn_popup_ok);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
